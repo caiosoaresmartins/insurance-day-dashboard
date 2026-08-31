82b7b766e549aaed96d58b6707264feceeeefce4
Caio Soares <105523930+caiosoaresmartins@users.noreply.github.com>
2026-08-31 19:28:13 -0300

    refactor: make Mes do Seguro dashboard the main app

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import CampaignDashboard from './CampaignDashboard.jsx';
 import FullscreenControls from './FullscreenControls.jsx';
 import IntelligencePanel from './IntelligencePanel.jsx';
 import SeptemberCampaign from './SeptemberCampaign.jsx';
 import './global.css';
 import './campaign-layout.css';
 import './campaign-entry.css';
+import './mes-do-seguro.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CampaignDashboard />
     <FullscreenControls />
     <IntelligencePanel />
     <SeptemberCampaign />
